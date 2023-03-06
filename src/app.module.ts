0c7169972738f1a4a9ffbe97c84a97b9b2c15ba5
ArceDaniel <jonathandanielarce9@gmail.com>
2023-03-06 20:08:26 -0300

    adaptando el back para el deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import User from './users/entities/user.entity';
 
-
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Medialuna1',
-      database: 'BankSaintPatrick',
-    
-      entities:[User],
-      synchronize:true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('PGHOST'),
+        port: configService.get<number>('PGPORT'),
+        username: configService.get<string>('PGUSERNAME'),
+        password: configService.get<string>('PGPASSWORD'),
+        database: configService.get<string>('PGDATABASE'),
+        entities:[User],
+        synchronize:true,
+      }),
+      inject: [ConfigService],
     }),
     ConfigModule.forRoot({
       envFilePath: '.env',
@@ -30,3 +32,4 @@ import User from './users/entities/user.entity';
   providers: [AppService],
 })
 export class AppModule {}
+
