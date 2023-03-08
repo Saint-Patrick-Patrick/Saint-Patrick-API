1fea92fb0997c61ab3f1d03edead770b7d29e32b
Jonathan Daniel Arce Aguirre <112012487+ArceDaniel@users.noreply.github.com>
2023-03-08 16:39:42 -0300

    Update Cors.ts

--- src/constants/Cors.ts
+++ src/constants/Cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 
 export const CORS : CorsOptions ={
-    origin: ['http://localhost:5173', process.env.URL_FRONT],
+    origin: ['*'],
     allowedHeaders: ['Content-Type', 'Authorization',],
     methods: 'GET,PUT,POST,DELETE,OPTIONS,HEAD,PATCH',
     credentials:true,
