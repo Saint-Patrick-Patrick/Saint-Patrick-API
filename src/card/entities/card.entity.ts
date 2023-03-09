9c4078b7c78957692f6cb6b7085d66a58766fda3
ferchox920 <fernandoramones92@gmail.com>
2023-03-09 20:42:00 -0300

    first commit

--- src/card/entities/card.entity.ts
+++ src/card/entities/card.entity.ts
@@ -1,11 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import User from 'src/users/entities/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 @Entity({ name: 'cards' })
 export class Card {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'varchar', length: 19, nullable: true })
+  @Column({ type: 'varchar', length: 16, nullable: true })
   numberCard?: string;
 
   @Column({ type: 'varchar', length: 3, nullable: true })
@@ -13,4 +14,10 @@ export class Card {
 
   @Column({ type: 'varchar', length: 4, unique: true, nullable: true })
   securityPin?: string;
+
+  @ManyToOne(() => User, user => user.cards)
+  user: User;
+  
 }
+
+
